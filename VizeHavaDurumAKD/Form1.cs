bc46151c07a50fe7e19d133a0379b24bf0cde346
Ali Kaan DAL <1812901056@ogr.mehmetakif.edu.tr>
2021-04-25 01:45:02 +0300

    xml döküman oluşturuldu.kütüphane eklendi.Linkten veri alındı.root tanımlandı.Son olarak düğümleri (nodes) oluşturuldu.

--- VizeHavaDurumAKD/Form1.cs
+++ VizeHavaDurumAKD/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace VizeHavaDurumAKD
 {
     public partial class Form1 : Form
     {
+        string hava_durumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml"
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,10 @@ namespace VizeHavaDurumAKD
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(hava_durumu_link);
+            XmlElement root = doc1.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("sehirler");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
