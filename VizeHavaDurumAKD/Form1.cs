b9621281006a325cb5c3d78871f4aca506a64155
Ali Kaan DAL <1812901056@ogr.mehmetakif.edu.tr>
2021-04-25 02:02:11 +0300

    Timer için form kodları timer alanına kodlandı.Test edildi Program çalışıyor ve verileri çekebiliyor.

--- VizeHavaDurumAKD/Form1.cs
+++ VizeHavaDurumAKD/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,25 +29,47 @@ namespace VizeHavaDurumAKD
 
             foreach (XmlNode node in nodes)
             {
-                string Bolge = node["Bölge"].InnerText;
-                string ili = node["İl"].InnerText;
+                string Bogle = node["Bolge"].InnerText;
+                string ili = node["ili"].InnerText;
                 string Durum = node["Durum"].InnerText;
                 string mak_sicaklik = node["Mak"].InnerText;
                 string min_sicaklik = node["Min"].InnerText;
 
-                DataGridView row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
                 row.Cells[0].Value = Bogle;
                 row.Cells[1].Value = ili;
                 row.Cells[2].Value = Durum;
                 row.Cells[3].Value = mak_sicaklik;
                 row.Cells[4].Value = min_sicaklik;
                 dataGridView1.Rows.Add(row);
+               
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(hava_durumu_link);
+            XmlElement root = doc1.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("sehirler");
 
+            foreach (XmlNode node in nodes)
+            {
+                string Bogle = node["Bolge"].InnerText;
+                string ili = node["ili"].InnerText;
+                string Durum = node["Durum"].InnerText;
+                string mak_sicaklik = node["Mak"].InnerText;
+                string min_sicaklik = node["Min"].InnerText;
+
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = Bogle;
+                row.Cells[1].Value = ili;
+                row.Cells[2].Value = Durum;
+                row.Cells[3].Value = mak_sicaklik;
+                row.Cells[4].Value = min_sicaklik;
+                dataGridView1.Rows.Add(row);
+
+            }
         }
     }
 }
